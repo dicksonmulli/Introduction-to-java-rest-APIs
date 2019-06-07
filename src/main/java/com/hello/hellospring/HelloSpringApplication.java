9453f9720c10d5fcf5eaf26a01d9674620b471e3
Dickson <dickson.isaiah6@gmail.com>
2019-06-07 18:31:16 +0300

    build succesful but I cannot test on Postman

--- src/main/java/com/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/com/hello/hellospring/HelloSpringApplication.java
@@ -1,9 +1,12 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.controller.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = GreetingController.class)
 public class HelloSpringApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hello/hellospring/controller/GreetingController.java
+++ src/main/java/com/hello/hellospring/controller/GreetingController.java
@@ -1,19 +1,26 @@
 package com.hello.hellospring.controller;
 
 import com.hello.hellospring.model.Greeting;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
+@RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!!!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greet", method = RequestMethod.GET)
+    public String greet(@RequestParam(value="name", defaultValue="World") String name) {
+        //return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return "Hello";
+    }
+
+    @RequestMapping("/greetings")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
